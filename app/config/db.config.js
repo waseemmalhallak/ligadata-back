4a5f9ef486b55ae806b76929b35d03665c8ae8c6
Waseem Alhallak <alhallak.waseem@hotmail.com>
2021-04-05 12:43:31 +0300

    add db configurations

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "admin",
+    DB: "postgres",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
