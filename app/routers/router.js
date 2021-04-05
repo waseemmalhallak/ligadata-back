13d6dba17c2da22656c43cc9e9da299fab6bd718
Waseem Alhallak <alhallak.waseem@hotmail.com>
2021-04-05 13:13:18 +0300

    adding the routes

--- app/routers/router.js
+++ app/routers/router.js
@@ -5,4 +5,6 @@ const corona = require('../controllers/controller');
 
 router.get('/api/corona/all', corona.retrieveAllData);
 router.get('/api/corona/pagination', corona.pagination);
+router.get('/api/corona/list-countries', corona.pagination);
+router.get('/api/corona/countries-by-region', corona.pagination);
 module.exports = router;
